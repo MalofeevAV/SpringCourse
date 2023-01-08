4795ba26a9108c9d4bf2ef1a4aad2b6a8fc276db
MalofeevAV <9151532547@mail.ru>
2023-01-08 16:09:28 +0300

    lesson 5 - DI_2 + properties + List<Bean>

--- src/main/java/ru/malofeev/springcourse/MusicPlayer.java
+++ src/main/java/ru/malofeev/springcourse/MusicPlayer.java
@@ -1,13 +1,56 @@
 package ru.malofeev.springcourse;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MusicPlayer {
     private Music music;
+    private List<Music> musicList = new ArrayList<>();
+    private String name;
+    private int volume;
+
+    public MusicPlayer() {
+    }
 
     public MusicPlayer(Music music) {
         this.music = music;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public void setMusic(Music music) {
+        this.music = music;
+    }
+
+    public List<Music> getMusicList() {
+        return musicList;
+    }
+
+    public void setMusicList(List<Music> musicList) {
+        this.musicList = musicList;
+    }
+
     public void playMusic() {
         System.out.println("Playing: " + music.getSong());
+    }
+
+    public void playMusicList() {
+        for (Music music : musicList) {
+            System.out.println("Playing: " + music.getSong());
+        }
     }
 }
